ad821d7e4fe296ba14a52b6059dcaa685e79c5c7
MikeSollie <133972451+MikeSollie@users.noreply.github.com>
2024-04-17 11:46:44 +0200

    ADD: Added emergency button logic with state machine

--- MagazijnRobotArduino1.ino
+++ MagazijnRobotArduino1.ino
@@ -2,12 +2,24 @@
 #include "Src/JoystickModule/Joystick.h"
 #include "Src/MotorModule/Motor.h"
 
-Joystick joystick = Joystick(A0, A1, 30);
+Joystick joystick = Joystick(A2, A3, 30);
 Motor x_axisMotor = Motor(3, 12);
 Motor y_axisMotor = Motor(11, 13);
+int emergencyButtonPin = 2;
+
+
+enum RobotState{
+    automatic,
+    manual,
+    off
+};
+
+RobotState currentState = off;
 
 void setup()
 {
+    pinMode(emergencyButtonPin, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(emergencyButtonPin), turnOffRobot, FALLING);
     Serial.begin(9600);
     joystick.registerPins();
     x_axisMotor.registerPins();
@@ -16,4 +28,23 @@ void setup()
 
 void loop()
 {
+    switch (currentState){
+        case automatic:
+            break;
+        case manual:
+            handleManualInput();
+            break;
+        case off:
+            return;
+        default:
+            currentState = off;
+            break;
+    }
+}
+
+void turnOffRobot(){
+    currentState = off;
+}
+
+void handleManualInput(){
 }
